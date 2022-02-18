fc4f29db24ae0787e72f6265f0a1147bd3bca932
Yogendra <ryogendrap@gmail.com>
2022-02-18 11:56:51 -0600

    code

---
+++ src/codingtest/OrderDegree.java
@@ -0,0 +1,15 @@
+package codingtest;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class OrderDegree {
+    static int degree(List<Integer> arr){
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Integer []arr={1, 1, 2, 1, 2, 2};
+        degree(Arrays.asList(arr));
+    }
+}
