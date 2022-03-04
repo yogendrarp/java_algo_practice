68d4ac80a90f5a12dfa926677b7ea328e5fd51d4
Yogendra <ryogendrap@gmail.com>
2022-03-04 17:31:39 -0600

    Code

---
+++ src/Samsung.java
@@ -0,0 +1,9 @@
+public class Samsung {
+
+    public static void main(String[] args) {
+        char x=200; int k=0;
+        for(char i=0;i<2*x;i++){
+            System.out.println(++k);
+        }
+    }
+}
